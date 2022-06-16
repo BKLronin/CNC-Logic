6339c3c12f2be71deb38049193b68e5c2329aa4e
Thomas Herrmann <therrmann23@gmail.com>
2022-06-16 21:15:38 +0200

    Corrected pins

--- CNC-Logic.ino
+++ CNC-Logic.ino
@@ -1,5 +1,5 @@
-int PushButtonPin1 = 4;
-int PushButtonPin2 = 5;
+int PushButtonPin1 = 2;
+int PushButtonPin2 = 3;
 
 int mem1 = LOW;
 int mem2 = LOW;
@@ -10,14 +10,17 @@ int lastmem2 = LOW;
 int warteDelay = 50;
 int LastTime = 0;
 
-int xpro1 = 2;
-int xpro2 = 3;
+int xpro1 = 4;
+int xpro2 = 5;
 
-int relais1 = 0;
-int relais2 = 1;
+int relais1 = 6;
+int relais2 = 7;
 
 int relais1Set = LOW;
 int relais2Set = LOW;
+
+int laserPWMin = 9;
+int laserPWMout = 10;
 
 
 void setup() {
@@ -30,16 +33,27 @@ void setup() {
 
   pinMode(relais1, OUTPUT);
   pinMode(relais2, OUTPUT);
-}
-  // put your setup code here, to run once:
 
-void loop() {
+  pinMode(laserPWMin, INPUT);
+  pinMode(laserPWMout, OUTPUT);
+}
+  // ut your setup code here, to run once:
+
+
+void invertAnalogWrite(int pin, int value)
+{
+   analogWrite(pin, value);
+   TCCR1A = TCCR1A & ~B00110000; //switch off output B
+   TCCR1A |= B00110000;  //switch on the B output with inverted output
+}
+
+void loop() { 
   
   int reading1 = digitalRead(PushButtonPin1);
   int reading2 = digitalRead(PushButtonPin2);
   int reading3 = digitalRead(xpro1);
   int reading4 = digitalRead(xpro2); 
-  
+
  
   if (reading3 == LOW){   
     if (reading1 != lastmem1) 
@@ -72,6 +86,7 @@ void loop() {
   else
   {  digitalWrite(relais2, reading4);}
   
-  
+  int laser_in = analogRead(laserPWMin);
+  invertAnalogWrite(laserPWMout, laser_in); 
   
 }
